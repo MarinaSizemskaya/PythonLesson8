a5149e808fabcef1024c54d3026fb8a0daaf14a2
Marina <sizemskaya@gmail.com>
2023-04-20 18:31:25 +0300

    data create

---
+++ data_create.py
@@ -0,0 +1,20 @@
+def name_data():
+    name = input('Введите имя: ')
+    return name
+
+def surname_data():
+    surname = input('Введите фамилию: ')
+    return surname
+
+def phone_data():
+    phone = input('Введите номер телефона: ')
+    return phone
+
+def address_data():
+    address = input('Введите адрес: ')
+    return address
+
+def new_inform_data():
+    new_inform = input('Введите новые данные: ')
+    return new_inform
+    
